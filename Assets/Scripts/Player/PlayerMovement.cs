106e44bb0be9fb384fd15f658ac4bd604d35c6b8
rtfj-laptop <tfj.rubens@gmail.com>
2024-04-06 06:32:52 +0100

    child objects already orbiting the player, primary script of shooting light

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,10 +7,29 @@ public class PlayerMovement : MonoBehaviour
     public float    speed = 5.0f;
     public float    xInput;
     public float    yInput;
+    public float    orbitRadius = 1;
+    public float    orbitSpeed = 50f;
 
     void Start()
     {
-        
+        addChildrenOrbit();
+    }
+
+    void addChildrenOrbit() {
+        int numberOfChildren = transform.childCount;
+        for (int i = 0; i < numberOfChildren; i++)
+        {
+            Transform child = transform.GetChild(i);
+            // child.gameObject.AddComponent<RayToMouse>();
+            // child.gameObject.AddComponent<LineRenderer>();
+            child.gameObject.AddComponent<CircleCollider2D>();
+            OrbitMovement orbitScript = child.gameObject.AddComponent<OrbitMovement>();
+
+            orbitScript.orbitRadius = this.orbitRadius;
+            orbitScript.orbitSpeed = this.orbitSpeed;
+            orbitScript.initialAngleDegrees = (360f / numberOfChildren) * i;
+        }
+
     }
 
     public void Update()
